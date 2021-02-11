ce91740a67da47db2fdffacff45b40b7a2c3a449
juyonLee00 <ljy012900@gmail.com>
2021-02-11 23:33:12 +0900

    pathbox 기능 추가

---
+++ pathbox/app.js
@@ -0,0 +1,16 @@
+
+      state = 0;
+
+      function ClickImg() {
+        var element = document.getElementById("jsimg")
+        var vvalue = document.getElementById("jsimg").value;
+
+        console.log(vvalue);
+        if(state%2 == 0) {
+            element.style.opacity = 0.5;
+          }
+        else {
+            element.style.opacity = 1;
+          }
+        state = state+1; 
+      }
